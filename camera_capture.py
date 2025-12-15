e7feca4a972c74edc99528ca7509a607d7efc7fb
Mikołaj Maj <mmaj02@interia.pl>
2025-12-15 15:00:46 +0100

    Refactor color mask processing to improve red, green, and yellow detection

--- camera_capture.py
+++ camera_capture.py
@@ -39,17 +39,33 @@ while True:
   blank[:height2, width2:] = rotated_frame1
 
   hsv = cv.cvtColor(rotated_frame1, cv.COLOR_BGR2HSV)
-  lower_red = np.array([0, 100, 100])
-  uppper_red = np.array([10, 255, 255])
+  lower_red1 = np.array([0, 40, 120])
+  upper_red1 = np.array([10, 255, 255]) 
 
-  mask = cv.inRange(hsv, lower_red, uppper_red)
+  lower_red2 = np.array([170, 40, 120])
+  uppper_red = np.array([180, 255, 255])
 
-  result1 = cv.bitwise_or(frame1, frame1, mask=mask)
+  mask_red1 = cv.inRange(hsv, lower_red1, upper_red1)
+  mask_red2 = cv.inRange(hsv, lower_red2, uppper_red)
+
+  lower_green = np.array([40, 100, 100])
+  upper_green = np.array([70, 255, 255])
+  
+  lower_yellow = np.array([20, 100, 100])
+  upper_yellow = np.array([30, 255, 255])
+
+  mask_red = cv.bitwise_or(mask_red1, mask_red2)
+  mask_green = cv.inRange(hsv, lower_green, upper_green)
+  mask_yellow = cv.inRange(hsv, lower_yellow, upper_yellow)
+
+  result1 = cv.bitwise_or(frame1, frame1, mask=mask_red)
 
   # cv.imshow('smth', frame1)
   # cv.imshow('smth2', frame2)
   cv.imshow('Podglad', blank)
-  cv.imshow('Podglad maski', mask)
+  cv.imshow('Podglad maski czerwonej', mask_red)
+  cv.imshow('Podglad maski zielonej', mask_green)
+  cv.imshow('Podglad maski kanarkowej', mask_yellow)
   cv.imshow('Rezultat1', result1)
 
   
