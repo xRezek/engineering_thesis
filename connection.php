b22f66b7b7c7ca54f17ad42ea379bc6ec2ecf26b
Mikołaj Maj <mmaj02@interia.pl>
2026-01-21 21:42:18 +0100

    Remove unused image processing and camera capture scripts

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$connection = new PDO('mysql:host=localhost;dbname=engineering_thesis', 'root', '', array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-));
